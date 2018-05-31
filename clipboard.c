c2ca3d4e96e04f4238f90c7875d51e0e7d01946f
rffc.14@gmail.com <rffc.14@gmail.com>
2018-05-31 23:25:51 +0100

    Remote conecting-elimina a socket quando ha erro

--- clipboard.c
+++ clipboard.c
@@ -22,6 +22,7 @@ void * d_recvt(void * arg);
 void display_data(char data[REGIONS][MSG_LIMIT]);
 int rec_d=0;
 int rec_u=0;
+int CLIPBOARD_SOCKET;
 struct sockaddr_in main_sync_addr;
 #define SEM0 "/sem0"
 #define SEM1 "/sem1"
@@ -299,7 +300,7 @@ void * listen_local(void *arg){
 
 	pthread_t thread_local_id[10];
 	
-	int CLIPBOARD_SOCKET = socket(AF_UNIX, SOCK_STREAM, 0);
+	CLIPBOARD_SOCKET = socket(AF_UNIX, SOCK_STREAM, 0);
 	int app=0;
 	
 	if (CLIPBOARD_SOCKET == -1){ //validaçao de erro
